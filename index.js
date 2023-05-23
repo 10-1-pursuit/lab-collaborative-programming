39fff161d7777431af985aeab15f18b6139620bb
ShakaPursuit <shakalivngstone@pursuit.org>
2023-05-23 19:54:21 -0400

    Made changes to third function

--- index.js
+++ index.js
@@ -23,24 +23,41 @@ sortByStringLength(sentence)
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-const string="Hello"
-function textScroller(word) {
-  for(i=0; i < word.length; i++){
- newArray=string.split("");//to split character in array
-    newArray[0]= string[4]//repace last index with first index
-          newArray[4]= string[0]
-  }
-  return[ newArray.join("")]//to remove the comma
+const string= "Hello"
 
+
+function textScroller(word) {
+  const scramblerFunc = () => {
+    for (i = 0; i < word.length; i++) {
+
+
+      newArray = string.split("");//to split character in array
+      newArray[0] = string[4]//replace first index of new array with last index of string
+      newArray[4] = string[0]//replace  last index of new array with first index of string
+    }
+    return [newArray.join("")]//to remove the commas and pust back into array
+  }
+  return scramblerFunc()
 
 }
 console.log(textScroller(string))
+
+
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+const numbers = [3, 8, 44, -33, 20, 400, 6, 16, 2, 2, 2, 200];
+
+function betweenExtremes(num) {
+
+  for(i=0; i<num.length; i++){
+
+    return (Math.max(...numbers)-(Math.min(...numbers)))
+  }
+}
+console.log(betweenExtremes(numbers))
 
 /**
  * Returns the difference between the largest and smallest number in the array
