f41acfd17a0ed297a54ab8bf7645053a10c3b2cc
Jeremy Montes <jeremymontes@pursuit.org>
2023-05-23 19:17:33 -0400

    Natyka and I solved third problem

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function sortByStringLength(words) {
   return words.sort((a, b) => a.length - b.length);
 }
 
-
 /**
  * Returns an array of the word in all scrolling positions.
  * @param {String} word - A string.
@@ -17,14 +16,33 @@ function sortByStringLength(words) {
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+//   let wordsScroller = [];
+//   let characterArray = word.split("");
+//   let firstLetter = characterArray.shift();
+// characterArray.push(firstLetter)
+// wordsScroller.push(characterArray)
+//   return wordsScroller;
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+  let emptyArr = []
+ for(let num of numbers){
+  if(typeof num === "string"){
+    return numbers
+  }
+}
+let sort = numbers.sort((a,b)=> a-b)
+let firstNumber = sort.shift()
+
+let lastNumber = sort.pop()
+return lastNumber - firstNumber
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
