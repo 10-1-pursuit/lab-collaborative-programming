e8737cc092f59a8a630047b7ac331564290f5629
Yanako Lajara <yanakolajara@pursuit.org>
2023-05-24 01:31:54 -0400

    functions fixed

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(array) {
+  return array.sort((a, b) => a.length - b.length);
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +16,48 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+  let wordArr = word.split('')
+  let y = word.length
+  let newArr = []
+  for(let x = 0; x < y; x++){
+    let temp = wordArr[0]
+    let newWord = NaN
+    wordArr.splice(0,1)
+    wordArr.push(temp)
+    newWord = wordArr.join('')
+    newArr.push(newWord)
+  }
+  return newArr;
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(arr) {
+  min = 9999999999
+  max = 0
+  let allNum = true
+  arr.forEach((num) => {
+    if(typeof(num) !== 'number'){
+      allNum = false
+    }
+    if(num > max){
+      max = num;
+    }
+    if(num < min){
+      min = num;
+    }
+    return;
+  })
+  if(allNum){
+    return max - min
+  }else{
+    return arr;
+  }
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +67,22 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  let messageArr = message.split('')
+  let morseArr = []
+  let keys = Object.keys(dictionary)
+  for(let x of messageArr){
+    const y = keys.find(letter => letter.toLowerCase() === x.toLowerCase())
+    if(!!y){
+      morseArr.push(`${dictionary[y]} `)
+    }
+  }
+  morseArr = morseArr.join('')
+  morseArr = morseArr.split('')
+  morseArr.pop()
+
+  return morseArr.join('')
+}
 
 module.exports = {
   sortByStringLength,
