37bd83da9e60238be09435e21dade627c4461945
keniaDE <decoteaukenia@gmail.com>
2023-05-30 16:08:30 -0400

    kenia and iris completed lab

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const morseCodeDictionary = require("./morse-code-dictionary.json");
-
+//Iris Beckham and Kenia Decoteau 
 /**
  * Returns an array of all of the words sorted by length, shortest first
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(stringArr) {
+  const sortedStrings = [];
+ stringArr.sort((a, b) => a.length - b.length)
+  return stringArr;
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +18,33 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+//hello -> elloH
+function textScroller(word) {
+  const scrollArray =[];
+  const letters = word.split("")
+  console.log(word)
+  for(let i = 0; i < word.length; i++){
+    letters.push(letters.shift())
+    scrollArray.push(letters.join(''))
+    console.log(letters)
+      }
+  console.log(scrollArray)
+  return scrollArray;
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+  const high = Math.max(...numbers);
+  const low = Math.min(...numbers);
+  let diff = high -low ;
+
+   return diff || numbers;
+
+ }
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +54,16 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(string, dictionary) {
+  let result = [];
+  let words = string.split(" ");
+  let letters = words.join('').split('');
+  for(let letter of letters){
+    result .push(dictionary[letter.toUpperCase()]);
+    console.log(`letter: ${letter} morse: ${dictionary[letter.toUpperCase()]}`)
+  }
+  return result.join(" ");
+ }
 
 module.exports = {
   sortByStringLength,
